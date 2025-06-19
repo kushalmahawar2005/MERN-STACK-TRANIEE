1add2fd0f21292dfbb9638d8174fbf8314c7f400
Kushal mahawar <kushalmahawar71@gmail.com>
2025-06-19 10:48:27 +0530

    Change in files directory

---
+++ Learning/Js Learning/kushal.js
@@ -0,0 +1,11 @@
+//  DOM - Document Object Model --- Used to access and manipulate the HTML elements
+let elem = document.getElementById('click');
+    console.log(elem);
+
+
+let elemClass = document.getElementsByClassName("conatiner")
+
+
+
+
+
