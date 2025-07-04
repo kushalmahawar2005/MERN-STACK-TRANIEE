e9d9c3ea1c897a67c09d856fbf0ecb5630f5006d
Kushal mahawar <kushalmahawar71@gmail.com>
2025-07-04 10:36:03 +0530

    Day 16 Query Selector

--- Learning/Day 14 Functions in Js/index.js
+++ Learning/Day 14 Functions in Js/index.js
@@ -99,8 +99,150 @@
 // const multiply = (x , y) => x * y;
 //  console.log(multi(3 , 4 , multiply));
 
-function isEven (num) {
-    return num % 2 == 0;
+// function isEven (num) {
+//     return num % 2 == 0;
+// }
+
+// console.log(isEven(3));
+
+// let arr = [1 , 2 ,4 ,5 ];
+// document.write(arr.reverse());
+
+
+
+// let fruits = ["Anish" , "Akshat" , "Kushal" , "Anish" , "Akshat" , "Kushal"];
+// fruits.push("Dev");
+// fruits.pop("Dev");
+//  console.log(fruits.toString());
+//  console.log(fruits.shift("Anish"));
+//  console.log(fruits);
+//  fruits.unshift("Anish");
+// console.log(fruits);
+
+
+// let a = [1 ,2 ,2 ,4 ,5, 6,7 ];
+// a.splice(3 ,1 , 8 );
+// a.splice(5 , 2 , 9 , 10 , 7);
+// console.log(a);
+
+
+
+// setTimeout(function(){
+//     console.log("Hey Akshat");
+// }, 3000);
+
+// setIntervel(function(){
+//     console.log("Hey Anish, How are you?");
+// }, 2000);
+
+
+    // setInterval(
+    //     function() {
+    //         console.log("Hello!" + "<br>");
+    //     }, 2000
+    // )
+
+//     const add = (a , b) => {
+//         console.log( a + b);
+//     } 
+// add(5, 10);
+
+// (function(){
+//     console.log("chinar");
+// })();
+
+// function add (a , b , operation){
+//     return operation(a , b);
+// }
+
+// const add = (a , b) => a + b;
+// add(2 , 4 , add);
+
+
+// function isEven(num) {
+//     return num % 2 == 0;
+
+// }
+// console.log(isEven(104));
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// console.log(reverseString("Heelo Bhai Moja me!"));
+
+// function countVovels(str) {
+//     const vowels = "aeiouAEIOU";
+//     let count = 0;
+//     for (let char of str){
+//         if (vowels.includes(char)) 
+//         {
+//             count++;
+//         }
+
+//     }
+//     return count;
+// }
+
+// console.log(countVovels("Hello World! This is a test string.")); // Output: 10
+
+//DOM Manipulation
+
+// // let a = document.getElementsById("a").innerHTML.style.color = "red";
+// // let b = document.getElementsByClassName("'c'")[0].innerHTML.style.color = "blue";
+
+// // document.write(a);
+// // document.write(b);
+
+// let a  = document.getElementById("a");
+// a.style.color = "red";
+// a.style.backgroundColor = "yellow";
+// a.innerHTML = "Bhoot aya!!";
+
+// let b = document.getElementsByTagName("p")[0];
+// b.style.color = "green";
+
+// function hello() {
+//    let a = document.getElementById("H");
+//     a.style.backgroundColor = "black";
+//     a.style.color = "white";
+//     a.innerHTML = "Clicked!";
+   
+// };
+
+
+
+
+// function Kushal() {
+//     let a = document.getElementById("name").innerText;
+//     document.write("Hello"+ " " + a);
+//     a.style.color = "black";
+
+// }
+
+
+// function isArmstrong(num) {
+//     let sum = 0;
+//     let temp = num;
+//     const n = num.toString().length;
+//     while (temp > 0) {
+//         const digit  = temp % 10;
+//         sum += Math.pow(digit, n);
+//         temp = Math.floor(temp / 10);
+//     }
+//     return sum === num;
+// }
+
+// console.log(isArmstrong(153));
+
+function isStrong(num) {
+    let sum = 0;
+    let temp = num;
+    while (temp > 0) {
+        let factorial = 1;
+        const digit = temp % 10;
+        sum += factorial(digit);
+        temp = Math.floor(temp / 10);
+    }
 }
 
-console.log(isEven(3));
+console.log(isStrong(145)); // Output: true
---
+++ Learning/Day 15 js Query Selector/index.js
@@ -0,0 +1,61 @@
+// const element = document.querySelector('#myDiv');
+// console.log(element.innerTextContent);
+// const element = document.querySelector('.myClass');
+// console.log(element.innerTextContent);
+
+// let a = document.getElementById('a');
+// a.innerHTML = "<b> Hello Kushal <b>";
+// a.style.color = "skyblue";
+// a.style.backgroundColor = "black";
+
+// // --------------Class Liist----------------
+// 1. add('className') - Adds a class to the element.
+// 2. remove('className') - Removes a class from the element.
+// 3. toggle('className') - Toggles a class on the element (adds it if not present, removes it if present).
+
+
+// let a = document.getElementById('content');
+// let b = document.getElementById('add');
+// let c = document.getElementById('remove');
+// let d = document.getElementById('toggle');
+
+// b.onclick = function() {
+//     a.classList.add("Highlight");
+   
+
+// };
+// c.onclick = function() {
+//     a.classList.remove("Highlight");
+    
+// }
+
+// d.onclick = function() {
+//     a.classList.toggle("Highlight");
+   
+// };
+
+
+// function printFibbonaci(n) {
+//     let a = 0 , b = 1;
+//     console.log("Fibonacci Series:");
+//     for (let i = 0; i < 10; i++){
+//         console.log(a);
+//         let next = a + b;
+//         a = b;
+//         b = next;
+//     }
+// }
+
+// printFibbonaci(5);
+
+
+function pallindrom(str) {
+   
+    let b = " ";
+    for (let i = str.length - 1; i>= 0 ; i--){
+        b += str[i];
+    }
+    console.log(b);
+}
+
+pallindrom("kushal");
