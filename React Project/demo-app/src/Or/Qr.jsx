f802fcaccf6aedaaf0508b73748e1d40a11536e6
Kushal mahawar <kushalmahawar71@gmail.com>
2025-07-20 23:56:34 +0530

    Practice part added

--- React Project/demo-app/src/Or/Qr.jsx
+++
@@ -1,9 +0,0 @@
-import './Qr.css';
-
-export default function Qr() {
-    return (
-        <div className='Container'>
-            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/seo/App_download_banner.png" alt="container" className="Qr-container"  />
-        </div>
-    );
-}
--- React Project/demo-app/src/cities/Cities.jsx
+++
@@ -1,9 +0,0 @@
-import './Cities.css';
-
-export default function Cities({ data }) {
-    return (
-        <div className="city-card">
-            <p>{data}</p>
-        </div>
-    );
-}
