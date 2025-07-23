8508bd3c1e16e2b58753d60e8405938afec8541b
Kushal Mahawar <kushalmahawar71@gmail.com>
2025-07-23 22:57:14 +0530

    Extra Work ADDED

--- React Js Day 27-28/src/Day 31/UseRef.js
+++ React Js Day 27-28/src/Day 31/UseRef.js
@@ -1,46 +1,46 @@
-// import React, {useRef , useState } from 'react';
-
-// export default function UseRef() {
-//     const clickCounterRef = useRef(0);
-//     const [renderCount , setRenderCount] = useState(0);
-
-//     const handleClick = () => {
-//         clickCounterRef.current += 1;
-//         console.log(`button clicked ${clickCounterRef.current} times`);
-//     };
-
-
-//     const reRender = () => setRenderCount(renderCount + 1);
-
-//     return (
-//         <div>
-//             <h1>Using useRef for Mutable Values</h1>
-//             <button onClick = {handleClick} >Click Me</button>
-//             <button onClick = {reRender}>Re-render</button>
-//             <p>Click count (in console only):{clickCounterRef.current }</p>
-//             <p>Render count : {renderCount}</p>
-//         </div>
-//     );
-// }
-
-
-
-import React, { useRef } from 'react';
+import React, {useRef , useState } from 'react';
 
 export default function UseRef() {
-    const inputRef = useRef(null);
+    const clickCounterRef = useRef(0);
+    const [renderCount , setRenderCount] = useState(0);
 
-   const handleFocous = () => {
-    inputRef.current.focus();
-   };
+    const handleClick = () => {
+        clickCounterRef.current += 1;
+        console.log(`button clicked ${clickCounterRef.current} times`);
+    };
 
 
+    const reRender = () => setRenderCount(renderCount + 1);
 
-   return (
-    <div>
-        <h1>Accessing DOM Element with useRef</h1>
-        <input ref = {inputRef} type = 'text' placeholder='click button to focous' />
-        <button onClick = {handleFocous}>Focous Input</button>
-    </div>
-   );
+    return (
+        <div>
+            <h1>Using useRef for Mutable Values</h1>
+            <button onClick = {handleClick} >Click Me</button>
+            <button onClick = {reRender}>Re-render</button>
+            <p>Click count (in console only):{clickCounterRef.current }</p>
+            <p>Render count : {renderCount}</p>
+        </div>
+    );
 }
+
+
+
+// import React, { useRef } from 'react';
+
+// export default function UseRef() {
+//     const inputRef = useRef(null);
+
+//    const handleFocous = () => {
+//     inputRef.current.focus();
+//    };
+
+
+
+//    return (
+//     <div>
+//         <h1>Accessing DOM Element with useRef</h1>
+//         <input ref = {inputRef} type = 'text' placeholder='click button to focous' />
+//         <button onClick = {handleFocous}>Focous Input</button>
+//     </div>
+//    );
+// }
