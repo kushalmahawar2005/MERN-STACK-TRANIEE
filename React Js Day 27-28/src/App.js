09ce994578c3fc2ed93de6b7f178b4c3e4ea04e2
Kushal Mahawar <kushalmahawar71@gmail.com>
2025-07-25 22:40:00 +0530

    Day 33 React JS

--- React Js Day 27-28/src/App.js
+++ React Js Day 27-28/src/App.js
@@ -3,12 +3,13 @@
 // import Kushal from './kushal';
 // import Update from './Day 30/UseEffect'
 // import UseContext from './Day 30/UseContext'; 
-import UseRef from './Day 31/UseRef';
+// import UseRef from './Day 31/UseRef';
+import Fetch from "./Day 34/Custom API Hook/Fetch";
 
 function App() {
   return (
     
-    <UseRef />
+   <Fetch />
   );
 }
 
