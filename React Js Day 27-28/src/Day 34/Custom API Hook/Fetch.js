c0395fb16cae9d821d7ff4a40214f0c843a7ad66
Kushalmahawar2005 <Kushalmahawar2005@gmail.com>
2025-07-25 22:40:00 +0530

    Day 33

---
+++ React Js Day 27-28/src/Day 34/Custom API Hook/Fetch.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function useFetch(url) {
+    const [data, setData] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+
+
+React.useEffect(() => {
+    fetch(url) 
+    .then((responce) => responce.json())
+    .then((data) => {
+        setData(data);
+        setLoading(false);
+    });
+},[url]);
+
+return {data, loading};
+}
+
+export default function Fetch() {
+    const {data, loading} = useFetch('https://catfact.ninja/fact');
+
+
+
+return (
+    <div style = {{ padding : "16p"}}>
+        <h2>Data Fetch with Custom Hook</h2>
+        {loading ? <p>Loading .... </p> : <p>Fact About Cat : {JSON.stringify(data.fact)} </p>  }
+        {console.log(data)}
+        </div>
+
+
+
+
+);
+
+}
